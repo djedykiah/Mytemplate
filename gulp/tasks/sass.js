af1d1f4fa8a09e95db99c0c77d287f2b3ba8ad21
Anton.f <felseror@gmail.com>
2018-10-06 19:42:42 +0300

    added postcss-short

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,12 +1,16 @@
 'use strict';
 
-module.exports = function() {
+module.exports = function() { 
+	const postcss = require('gulp-postcss');
 	$.gulp.task('sass', function() {
 		return $.gulp.src('./src/style/app.scss')
 			.pipe($.gp.sourcemaps.init())
-			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' }))
+			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' })) 
 			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
-			.pipe($.gp.sourcemaps.write())
+			.pipe($.gp.sourcemaps.write()) 
+			.pipe(postcss([
+				require('postcss-short')()
+			]))
 			.pipe($.gulp.dest($.config.root + '/css'))
 			.pipe($.browserSync.stream());
 	})
