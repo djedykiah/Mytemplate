2dfb8badee109379df33306debe14f6488ddc535
Anton.f <felseror@gmail.com>
2018-10-07 17:49:54 +0300

    srabotalo

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -7,37 +7,45 @@ module.exports = function() {
   shortConfig[val] = { skip: '_' };
 }); 
 	$.gulp.task('sass', function() {  
-		var postcss = require('gulp-postcss'); 
-		var sorting = require('postcss-sorting')
+		const postcss = require('gulp-postcss');  
+		const caralho = require('postcss-caralho');
+		const words = require('curse-words-common');
 		var processors = [ 
+			caralho(words), 
 			require('postcss-short')(shortConfig), 
 			require('rucksack-css')({
 				autoprefixer: false
-			}),
+			}), 
+  		require("postcss-reporter")({ clearReportedMessages: true }),
 			require('postcss-font-magician')(), 
-			require('css-mqpacker')(), 
-			require('postcss-pxtorem')(), 
-			sorting({
-				"order": [
-					"custom-properties",
-					"dollar-variables",
-					"declarations",
-					"at-rules",
-					"rules"
-					],
-					"properties-order": [ "position", "top", "right", "bottom", "left", "z-index", "display", "float", "width", "height", "font", "font-size", "line-height", "color", "text-align", "background-color", "background", "border", "border-radius", "box-shadow", "padding", "margin", "opacity"]
-			})
+			require('css-mqpacker')(),  
+			require('postcss-fixes')(), 
+			require('postcss-hocus'),
+			require('postcss-pseudo-content-insert'),
+			require('postcss-insert')(), 
+			require('postcss-merge-rules')(), 
+			require('postcss-single-line'),
+			require('postcss-pxtorem')(),  
+			require('css-declaration-sorter')({
+				order: 'concentric-css'
+			}), 
+			require('postcss-preset-env')(Object.assign({
+				stage: 0
+			}))
+
 			
 		]
 		return $.gulp.src('./src/style/app.scss')
-			.pipe($.gp.sourcemaps.init())
+			.pipe($.gp.sourcemaps.init()) 
+			.pipe(postcss(processors,
+				{
+					syntax: require('postcss-scss')
+				}
+				
+				))
 			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' })) 
 			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig })) 
-			.pipe(postcss(processors,
-			{
-				syntax: require('postcss-scss')
-			}
-			))
+	
 			.pipe($.gp.sourcemaps.write()) 
 			.pipe($.gulp.dest($.config.root + '/css'))
 			.pipe($.browserSync.stream());
