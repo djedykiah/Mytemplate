73eddfd96bbbe1e8a67e8f87ed9ba9b411579c32
Anton.f <felseror@gmail.com>
2018-10-07 09:36:50 +0300

    beforesorting

--- gulp/tasks/sass.js
+++ gulp/tasks/sass.js
@@ -1,16 +1,34 @@
 'use strict';
 
 module.exports = function() { 
-	const postcss = require('gulp-postcss');
-	$.gulp.task('sass', function() {
+
+	const shortConfig = {};
+['border', 'borderRadius', 'color', 'fontSize', 'position', 'size', 'spacing'].forEach((val) => {
+  shortConfig[val] = { skip: '_' };
+}); 
+	$.gulp.task('sass', function() {  
+		var postcss = require('gulp-postcss'); 
+		var processors = [ 
+			require('postcss-short')(shortConfig), 
+			require('rucksack-css')({
+				autoprefixer: false
+			}),
+			require('postcss-font-magician')(), 
+			require('css-mqpacker')(), 
+			require('postcss-pxtorem')(), 
+			require('postcss-sorting')({
+				'order': [
+					'alphabetical'
+				]
+			})
+			
+		]
 		return $.gulp.src('./src/style/app.scss')
 			.pipe($.gp.sourcemaps.init())
 			.pipe($.gp.sass()).on('error', $.gp.notify.onError({ title: 'Style' })) 
-			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig }))
+			.pipe($.gp.autoprefixer({ browsers: $.config.autoprefixerConfig })) 
+			.pipe(postcss(processors))
 			.pipe($.gp.sourcemaps.write()) 
-			.pipe(postcss([
-				require('postcss-short')()
-			]))
 			.pipe($.gulp.dest($.config.root + '/css'))
 			.pipe($.browserSync.stream());
 	})
